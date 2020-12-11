690a54490d29553e91e14a0feeb85700a071f9a8
Elverarroyave <elver.arroyave@gmail.com>
2020-12-11 17:27:05 -0500

    Creamos rutas y componentes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+//Componentes
+import Deshboard from './components/Deshboard';
+import CreateUser from './components/CreateUser'
+import Login from './components/Login'
+import Navegation from './components/Navegation';
+
 
 function App() {
   return (
+    //La sintaxis utilizada se llama JSX
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route path = "/" component = {Navegation}/>
+        <Route exact path = "/login" component = {Login}/>
+        <Route exact path = "/user" component = {CreateUser}/>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Deshboard.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Deshboard = () => {
+    return ( <h1>Desde Deshboar</h1> );
+}
+ 
+export default Deshboard;
